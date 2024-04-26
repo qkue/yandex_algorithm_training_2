1acb3ad35770ae67c2a6b1188ec398d3420eaf05
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 16:00:53 +0300

    Update solution.py

--- Division B/Exercise 6/D/solution.py
+++ Division B/Exercise 6/D/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+a, k, b, m, x = map(int, input().split())
+
+left = 0
+right = x
+
+while left < right:
+    mid = (left + right) // 2
+    if (a * (mid - mid // k) + (b * (mid - mid // m))) >= x:
+        right = mid
+    else:
+        left = mid + 1
+
+print(left)
