217a2335eef5be04f6ad580e10f9c828168a0b0e
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 15:57:11 +0300

    Update solution.py

--- Division B/Exercise 6/C/solution.py
+++ Division B/Exercise 6/C/solution.py
@@ -1,2 +1,20 @@
 # Python 3.12.1
 
+a, b, c, d = map(int, input().split())
+
+def check(x):
+    if a > 0:
+        return (a*x*x*x + b*x*x + c*x + d) <= 0
+    else:
+        return (a*x*x*x + b*x*x + c*x + d) >= 0
+        
+right = 2000
+left = -2000
+
+while (left + 0.000000001) < right:
+    m = (left + right) / 2
+    if check(m):
+        left = m
+    else:
+        right = m
+print(left) 
