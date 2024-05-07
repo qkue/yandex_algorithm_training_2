5194c59191f88118745530515e9284941ec201b3
qkue <130838079+qkue@users.noreply.github.com>
2024-05-07 14:28:47 +0300

    Update solution.py

--- Division B/Exercise 2/A/solution.py
+++ Division B/Exercise 2/A/solution.py
@@ -1,2 +1,17 @@
 # Python 3.12.1
 
+number = int(input())
+
+max_element = 0
+count_elements = 0
+
+while number != 0:
+    if number > max_element:
+        max_element = number
+        count_elements = 1
+    elif number == max_element:
+        count_elements += 1
+
+    number = int(input())
+  
+print(count_elements)
