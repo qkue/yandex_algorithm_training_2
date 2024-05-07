8a82ace63a1d939f3390994f297b8e7cad140476
qkue <130838079+qkue@users.noreply.github.com>
2024-05-07 15:10:32 +0300

    Update solution.py

--- Division B/Exercise 2/B/solution.py
+++ Division B/Exercise 2/B/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+arr = list(map(int, input().split()))
+result = 0
+for house in range(len(arr)):
+    if arr[house] == 1:
+        dist = 20
+        for building in range(len(arr)):
+            if arr[building] == 2 and abs(house - building) < dist:
+                dist = abs(house - building)
+
+        if result < dist:
+            result = dist
+
+print(result)
