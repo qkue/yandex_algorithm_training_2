bdcc267f45f83edb539a173d5f71bc7c04a7b401
qkue <130838079+qkue@users.noreply.github.com>
2024-05-07 23:20:59 +0300

    Update solution.py

--- Division B/Exercise 3/C/solution.py
+++ Division B/Exercise 3/C/solution.py
@@ -1,2 +1,12 @@
 # Python 3.12.1
 
+arr_1 = list(map(int, input().split()))
+ans = []
+already_see = set()
+
+for num in range(len(arr_1)):
+    if arr_1[num] not in already_see and arr_1[num] not in set(arr_1[num+1:]):
+        ans.append(arr_1[num])
+    already_see.add(arr_1[num])
+
+print(*ans)
