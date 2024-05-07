638a0c60e3f004d65daf90abf7fada263ba0300d
qkue <130838079+qkue@users.noreply.github.com>
2024-05-07 22:26:20 +0300

    Update solution.py

--- Division B/Exercise 2/E/solution.py
+++ Division B/Exercise 2/E/solution.py
@@ -1,2 +1,14 @@
 # Python 3.12.1
 
+n = int(input())
+arr = list(map(int, input().split()))
+ans = 0
+max_folder = 0
+
+for folder in arr:
+    if folder > max_folder:
+        max_folder = folder
+        
+    ans += folder
+ans -= max_folder
+print(ans)
