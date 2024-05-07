63ab98dfc58f426adad7229eaaf8d5d5490fc09a
qkue <130838079+qkue@users.noreply.github.com>
2024-05-07 15:36:13 +0300

    Update solution.py

--- Division B/Exercise 2/C/solution.py
+++ Division B/Exercise 2/C/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+word = input()
+answer = 0
+    
+if len(word) > 1:
+    left = 0
+    right = -1
+    for sym in range(len(word) // 2):
+        if word[left] != word[right]:
+            answer += 1
+        left += 1
+        right -= 1
+
+print(answer)
