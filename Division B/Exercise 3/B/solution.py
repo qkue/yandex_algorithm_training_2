cb2edf6a2b603352f4e363d370ca16e7238088a7
qkue <130838079+qkue@users.noreply.github.com>
2024-05-07 22:46:41 +0300

    Update solution.py

--- Division B/Exercise 3/B/solution.py
+++ Division B/Exercise 3/B/solution.py
@@ -1,2 +1,14 @@
 # Python 3.12.1
 
+arr_1 = list(map(int, input().split()))
+ans = []
+we_meet = set()
+
+for num in arr_1:
+    if num in we_meet:
+        ans.append('YES')
+    else:
+        ans.append('NO')
+    we_meet.add(num)
+    
+print('\n'.join(ans))
