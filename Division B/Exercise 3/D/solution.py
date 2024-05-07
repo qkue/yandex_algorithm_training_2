a9a3fc5be8b881a3362ca163faf7e93e1527d174
qkue <130838079+qkue@users.noreply.github.com>
2024-05-08 00:33:29 +0300

    Update solution.py

--- Division B/Exercise 3/D/solution.py
+++ Division B/Exercise 3/D/solution.py
@@ -1,2 +1,20 @@
 # Python 3.12.1
 
+with open('input.txt', 'rt', encoding = 'utf-8') as file_input:
+    numbers = {num for num in range(1, int(file_input.readline().rstrip()))}
+
+    beatris = None
+    august = None
+
+
+    while beatris != 'HELP':
+        beatris = file_input.readline().rstrip()
+        if beatris != 'HELP':
+            beatris = set(map(int, beatris.split()))
+            august = file_input.readline().rstrip()
+            if august == 'YES':
+                numbers &= beatris
+            elif august == 'NO':
+                numbers -= beatris
+
+print(*sorted(numbers))
