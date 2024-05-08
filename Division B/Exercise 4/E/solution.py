c6d01059d6071efd1c830af614ec7426041de022
qkue <130838079+qkue@users.noreply.github.com>
2024-05-08 17:08:00 +0300

    Update solution.py

--- Division B/Exercise 4/E/solution.py
+++ Division B/Exercise 4/E/solution.py
@@ -1,2 +1,29 @@
 # Python 3.12.1
 
+forum = dict()
+order_message = dict()
+counter = 0
+
+n = int(input())
+for _ in range(n):
+    identificator = int(input())
+    if identificator == 0:
+        topic = input()
+        forum[topic] = forum.get(topic, 0) + 1
+        temp = input()
+        
+    else:
+        temp = input()
+        topic = order_message[identificator]
+        forum[topic] += 1
+
+    counter += 1
+    order_message[counter] = topic
+
+result = []
+cntr = 1
+for key, value in forum.items():
+    result.append((key, value, cntr))
+    cntr += 1
+result.sort(key = lambda p: (-p[1], p[2]))
+print(result[0][0])
