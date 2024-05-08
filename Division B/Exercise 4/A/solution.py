2a390b728424a9c2fd368705e3404ad5ff5d1bea
qkue <130838079+qkue@users.noreply.github.com>
2024-05-08 10:40:00 +0300

    Update solution.py

--- Division B/Exercise 4/A/solution.py
+++ Division B/Exercise 4/A/solution.py
@@ -1,2 +1,16 @@
 # Python 3.12.1
 
+boxes = dict()
+n = int(input())
+
+for _ in range(n):
+    key, value = map(int, input().split())
+    if key not in boxes:
+        boxes[key] = 0
+    boxes[key] += value
+
+answer = []
+for key in sorted(boxes):
+    answer.append(f'{key} {boxes[key]}')
+
+print('\n'.join(answer))
