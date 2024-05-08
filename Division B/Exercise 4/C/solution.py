a5dc60c0a02fec1884b5b1c0d8e34eb0ea07b13e
qkue <130838079+qkue@users.noreply.github.com>
2024-05-08 11:01:14 +0300

    Update solution.py

--- Division B/Exercise 4/C/solution.py
+++ Division B/Exercise 4/C/solution.py
@@ -1,2 +1,21 @@
 # Python 3.12.1
 
+words = dict()
+
+with open('input.txt', 'rt', encoding = 'utf-8') as file_input:
+    for word in file_input.read().split():
+        #word = line.rstrip()
+        if word not in words:
+            words[word] = 0
+        words[word] += 1
+
+sorted_words = []
+for key, value in words.items():
+    sorted_words.append((value, key))
+sorted_words.sort(key = lambda x: (-x[0], x[1]))
+
+answer = []
+for word in sorted_words:
+    answer.append(word[1])
+
+print('\n'.join(answer))
