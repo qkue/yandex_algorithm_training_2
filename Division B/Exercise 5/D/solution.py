a611a356ef754feefed1040a8967919eeb7fcd8f
qkue <130838079+qkue@users.noreply.github.com>
2024-05-09 12:31:21 +0300

    Update solution.py

--- Division B/Exercise 5/D/solution.py
+++ Division B/Exercise 5/D/solution.py
@@ -1,2 +1,14 @@
 # Python 3.12.1
 
+arr = input()
+parenthesis = 0
+flag_wrong = 0
+
+for element in arr:
+    if element == '(':
+        parenthesis += 1
+    else:
+        parenthesis -= 1
+    flag_wrong = min(parenthesis, flag_wrong)
+
+print('NO' if flag_wrong != 0 or parenthesis else 'YES')
