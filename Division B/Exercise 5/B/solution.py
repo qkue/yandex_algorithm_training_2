cb5e4a8d2952e17dbf871fa67cb45ff9aef07fd1
qkue <130838079+qkue@users.noreply.github.com>
2024-05-09 10:18:13 +0300

    Update solution.py

--- Division B/Exercise 5/B/solution.py
+++ Division B/Exercise 5/B/solution.py
@@ -1,2 +1,13 @@
 # Python 3.12.1
 
+n= int(input())
+arr = list(map(int, input().split()))
+current_sum = 0
+max_sum = arr[0]
+
+for element in arr:
+    current_sum += element
+    max_sum = max(max_sum, current_sum)
+    current_sum = max(current_sum, 0)
+
+print(max_sum)
