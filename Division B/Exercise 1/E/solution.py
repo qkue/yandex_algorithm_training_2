94d85839953b92669e712659d459dd3f7779178b
qkue <130838079+qkue@users.noreply.github.com>
2024-05-10 20:39:11 +0300

    Update solution.py

--- Division B/Exercise 1/E/solution.py
+++ Division B/Exercise 1/E/solution.py
@@ -5,9 +5,9 @@ x, y = map(int, input().split())
 
 if x >= 0 and y >= 0 and x + y <= d:
     print(0)
-else:
-    a = (1, (x - 0) ** 2 + (y - 0) ** 2)
-    b = (2, (x - d) ** 2 + (y - 0) ** 2)
-    c = (3, (x - 0) ** 2 + (y - d) ** 2)
-    ans = min([a, b, c], key = lambda p: (p[1], p[0]))
-    print(ans[0])
+else:    
+    a = ((x - 0) ** 2 + (y - 0) ** 2, 1)
+    b = ((x - d) ** 2 + (y - 0) ** 2, 2)
+    c = ((x - 0) ** 2 + (y - d) ** 2, 3)
+    ans = min([a, b, c])
+    print(ans[1])
