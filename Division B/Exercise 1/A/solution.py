4b484e289ff5ab3fef15b66c0b5e4c05f7f6fe48
qkue <130838079+qkue@users.noreply.github.com>
2024-05-10 09:38:25 +0300

    Update solution.py

--- Division B/Exercise 1/A/solution.py
+++ Division B/Exercise 1/A/solution.py
@@ -1,2 +1,22 @@
 # Python 3.12.1
 
+r = int(input()) # task
+i = int(input()) # interactor
+c = int(input()) # checker
+ans = None
+
+if i == 0 or i == 4:
+    if r != 0:
+        ans = 3
+    else:
+        ans = c if i == 0 else 4
+elif i == 1:
+    ans = c
+elif i == 6:
+    ans = 0
+elif i == 7:
+    ans = 1
+else:
+    ans = i
+
+print(ans)
