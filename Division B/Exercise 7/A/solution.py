87a26b65a47a176b01e50b89395af2b0d3127526
qkue <130838079+qkue@users.noreply.github.com>
2024-05-11 14:44:37 +0300

    Update solution.py

--- Division B/Exercise 7/A/solution.py
+++ Division B/Exercise 7/A/solution.py
@@ -1,2 +1,19 @@
 # Python 3.12.1
 
+d = int(input())
+answer = 0
+arr = sorted(tuple(map(int, input().split())) for _ in range(d))
+
+left = - 10 ** 10
+
+for i in range(d):
+    
+    x, y = arr[i]
+    if left < x:
+        left = x
+    elif left > y:
+        y = left
+    answer += (y - left)
+    left = y
+
+print(answer)
