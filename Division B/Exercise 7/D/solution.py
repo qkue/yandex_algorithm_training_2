168d029789294bc36cb23641e9652102da2b4717
qkue <130838079+qkue@users.noreply.github.com>
2024-05-12 13:57:30 +0300

    Update solution.py

--- Division B/Exercise 7/D/solution.py
+++ Division B/Exercise 7/D/solution.py
@@ -1,2 +1,26 @@
 # Python 3.12.1
 
+n, m = map(int, input().split())
+events = []
+
+for kitten in list(map(int, input().split())):
+    events.append((kitten, 0))
+
+for i in range(m):
+    L, R = map(int, input().split())
+    events.append((L, -1, i))
+    events.append((R, 1, i))
+events.sort()
+
+kitty = 0
+answer = [0] * m
+
+for event in events:
+    if event[1] == -1:
+        answer[event[2]] -= kitty
+    elif event[1] == 0:
+        kitty += 1
+    elif event[1] == 1:
+        answer[event[2]] += kitty
+
+print(*answer)
